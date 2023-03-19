65dd1226346c9ca470e1fab957080c56adf069ba
Lukasz Duda <lukasz.duda@nordicsemi.no>
2023-03-19 08:07:41 +0100

    Task 3: Introduce logger
    Signed-off-by: Lukasz Duda <lukasz.duda@nordicsemi.no>

--- src/main.c
+++ src/main.c
@@ -1,12 +1,14 @@
 /*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
- *
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(APP);
 
 void main(void)
 {
-	printk("Hello World! %s\n", CONFIG_BOARD);
+	LOG_INF("Hello World!");
+	LOG_DBG("Board: %s", CONFIG_BOARD);
 }
