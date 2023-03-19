7603c7e450788846bf5c8f7d002e785612eea6db
Lukasz Duda <lukasz.duda@nordicsemi.no>
2023-03-19 09:23:02 +0100

    Task 8: Control brightness - SW
    Signed-off-by: Lukasz Duda <lukasz.duda@nordicsemi.no>

--- src/main.c
+++ src/main.c
@@ -14,7 +14,14 @@
 #define LED_THREAD_STACK_SIZE 500
 #define LED_THREAD_PRIORITY   5
 
+#define PWM_THREAD_STACK_SIZE 500
+#define PWM_THREAD_PRIORITY   5
+
+#define PWM_LEVEL_MAX 10
+
 LOG_MODULE_REGISTER(APP);
+
+static volatile int pwm_level = 0;
 
 static void led_thread(void)
 {
@@ -30,6 +37,24 @@ static void led_thread(void)
 	}
 }
 
+static void pwm_thread(void)
+{
+	while(1) {
+		if (pwm_level) {
+			int active_time_ms   = pwm_level;
+			int inactive_time_ms = (PWM_LEVEL_MAX - 1) - active_time_ms;
+
+			dk_set_led_on(APP_LED);
+			k_msleep(active_time_ms);
+			dk_set_led_off(APP_LED);
+			k_msleep(inactive_time_ms);
+		}
+		else {
+			k_msleep(100);
+		}
+	}
+}
+
 static void button_handler(uint32_t button_state, uint32_t has_changed)
 {
 	int err;
@@ -40,9 +65,8 @@ static void button_handler(uint32_t button_state, uint32_t has_changed)
 	{
 		LOG_INF("Button 1 has been pushed.");
 
-		err = dk_set_led_off(APP_LED);
-		if (err) {
-			LOG_ERR("Failed to turn off the LED. Error %d", err);
+		if (pwm_level > 0) {
+			pwm_level--;
 		}
 	}
 
@@ -50,9 +74,8 @@ static void button_handler(uint32_t button_state, uint32_t has_changed)
 	{
 		LOG_INF("Button 2 has been pushed.");
 
-		err = dk_set_led_on(APP_LED);
-		if (err) {
-			LOG_ERR("Failed to turn on the LED. Error %d", err);
+		if (pwm_level < PWM_LEVEL_MAX) {
+			pwm_level++;
 		}
 	}
 }
@@ -85,3 +108,7 @@ void main(void)
 K_THREAD_DEFINE(led_tid, LED_THREAD_STACK_SIZE,
                 (k_thread_entry_t)led_thread, NULL, NULL, NULL,
                 LED_THREAD_PRIORITY, 0, 0);
+
+K_THREAD_DEFINE(pwm_tid, PWM_THREAD_STACK_SIZE,
+                (k_thread_entry_t)pwm_thread, NULL, NULL, NULL,
+                PWM_THREAD_PRIORITY, 0, 0);
